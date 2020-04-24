5dacde6f3f838fa0cf989a9709d958b297148d58
Nikki DelRosso <nikkidelrosso@gmail.com>
2020-04-23 19:39:04 -0700

    Updates

---
+++ precache-manifest.c02d445da328673495efd080f0b8a829.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5859044ebc67ea0eae5e92941392da8",
+    "url": "/react-oekaki/index.html"
+  },
+  {
+    "revision": "b0c88587c61c402d4e06",
+    "url": "/react-oekaki/static/css/main.ce3c424f.chunk.css"
+  },
+  {
+    "revision": "bb34f7af4dc3673f9978",
+    "url": "/react-oekaki/static/js/2.a5288bd2.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-oekaki/static/js/2.a5288bd2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b0c88587c61c402d4e06",
+    "url": "/react-oekaki/static/js/main.d27e8779.chunk.js"
+  },
+  {
+    "revision": "71e28739aac836878239",
+    "url": "/react-oekaki/static/js/runtime-main.766c4f15.js"
+  },
+  {
+    "revision": "2a1e6d1192818d6289e57e6ecb9464e7",
+    "url": "/react-oekaki/static/media/transparent.2a1e6d11.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8652db0a54f85d30cdc3a714ccdc779a.js"
+  "/react-oekaki/precache-manifest.c02d445da328673495efd080f0b8a829.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-oekaki/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
