d58dd4b1e947e1cb9bacb15c431b6329f7a3e999
Nikki DelRosso <nikkidelrosso@gmail.com>
2020-04-23 19:26:42 -0700

    Adding build directory

---
+++ precache-manifest.8652db0a54f85d30cdc3a714ccdc779a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55e85a5ed91ff71fb09fc60d16385637",
+    "url": "/index.html"
+  },
+  {
+    "revision": "72069f807a123a0311b9",
+    "url": "/static/css/main.1d5e90af.chunk.css"
+  },
+  {
+    "revision": "bb34f7af4dc3673f9978",
+    "url": "/static/js/2.a5288bd2.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.a5288bd2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "72069f807a123a0311b9",
+    "url": "/static/js/main.a4ba8f87.chunk.js"
+  },
+  {
+    "revision": "a2e1072f28440499bfbf",
+    "url": "/static/js/runtime-main.4b331846.js"
+  },
+  {
+    "revision": "2a1e6d1192818d6289e57e6ecb9464e7",
+    "url": "/static/media/transparent.2a1e6d11.svg"
+  }
+]);
